c33d6d77acaf556850213a4d93b56e3c260f3ee8
Max Thirouin <git@moox.io>
2026-05-28 08:36:21 +0200

    update to postcss v8

--- src/index.js
+++ src/index.js
@@ -1,81 +1,78 @@
-import postcss from 'postcss';
+'use strict';
 
-export default postcss.plugin('postcss-filter-plugins', ({
-    template = ({postcssPlugin}) => `Found duplicate plugin: ${postcssPlugin}`,
-    silent = false,
-    exclude = [],
-    direction = 'both',
-} = {}) => {
+const postcss = require('postcss');
+
+const PLUGIN_NAME = 'postcss-filter-plugins';
+const POSTCSS_VERSION = new postcss.Processor().version;
+
+const create = (opts = {}) => {
+    const {
+        template = ({postcssPlugin}) => `Found duplicate plugin: ${postcssPlugin}`,
+        silent = false,
+        exclude = [],
+        direction = 'both',
+    } = opts;
+
     const id = Math.random().toString();
-    let prev, next, both;
 
-    switch (direction) {
-    case 'both':
-        both = true;
-        break;
-    case 'backward':
-        prev = true;
-        break;
-    case 'forward':
-        next = true;
-        break;
-    }
+    return {
+        postcssPlugin: PLUGIN_NAME,
+        postcssVersion: POSTCSS_VERSION,
+        _id: id,
+        prepare(result) {
+            const plugins = result.processor.plugins;
 
-    const processor = (css, result) => {
-        const previousPlugins = [];
-        const nextPlugins = [];
-        const bothPlugins = [];
-        let filter = false;
-        let position = 0;
-
-        const detect = (list, plugin) => {
-            const name = plugin.postcssPlugin;
-            if (typeof name === 'undefined') {
-                position++;
-                return;
-            }
-            if (~list.indexOf(name)) {
-                if (!silent) {
-                    result.warn(template(plugin));
+            let selfIndex = -1;
+            for (let i = 0; i < plugins.length; i++) {
+                if (plugins[i]._id === id) {
+                    selfIndex = i;
+                    break;
                 }
-                result.processor.plugins.splice(position, 1);
-            } else {
-                list.push(name);
-                position ++;
             }
-        };
 
-        while (position < result.processor.plugins.length) {
-            const plugin = result.processor.plugins[position];
-            if (~exclude.indexOf(plugin.postcssPlugin)) {
-                position ++;
-                continue;
+            const seenBefore = [];
+            const seenAfter = [];
+            const seenBoth = [];
+            const toRemove = [];
+
+            for (let i = 0; i < plugins.length; i++) {
+                if (i === selfIndex) continue;
+                const plugin = plugins[i];
+                const name = plugin.postcssPlugin;
+                if (typeof name === 'undefined') continue;
+                if (name === PLUGIN_NAME) continue;
+                if (exclude.indexOf(name) !== -1) continue;
+
+                let list;
+                if (direction === 'both') {
+                    list = seenBoth;
+                } else if (direction === 'forward') {
+                    if (i < selfIndex) continue;
+                    list = seenAfter;
+                } else if (direction === 'backward') {
+                    if (i > selfIndex) continue;
+                    list = seenBefore;
+                } else {
+                    continue;
+                }
+
+                if (list.indexOf(name) !== -1) {
+                    if (!silent) result.warn(template(plugin));
+                    toRemove.push(i);
+                } else {
+                    list.push(name);
+                }
             }
-            if (plugin._id === id) {
-                position ++;
-                filter = true;
-                continue;
-            } else if (plugin.postcssPlugin === 'postcss-filter-plugins') {
-                position ++;
-                continue;
+
+            for (let i = toRemove.length - 1; i >= 0; i--) {
+                plugins.splice(toRemove[i], 1);
             }
-            if (both) {
-                detect(bothPlugins, plugin);
-                continue;
-            }
-            if (filter && next) {
-                detect(nextPlugins, plugin);
-                continue;
-            }
-            if (!filter && prev) {
-                detect(previousPlugins, plugin);
-                continue;
-            }
-            position ++;
-        }
+
+            return {};
+        },
     };
+};
 
-    processor._id = id;
+create.postcss = true;
 
-    return processor;
-});
+module.exports = create;
