fa93042fc155e09279485a265caa922c874eba36
Max Thirouin <git@moox.io>
2026-05-28 08:36:21 +0200

    update to postcss v8

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,59 +1,66 @@
-import test from 'ava';
-import postcss from 'postcss';
-import {name} from '../../package.json';
-import filter from '../';
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const postcss = require('postcss');
+const {name} = require('../../package.json');
+const filter = require('../');
 
 function getWarnings (filters) {
-    return postcss(filters).process('h1{}').warnings();
+    return postcss(filters).process('h1{}', {from: undefined}).warnings();
 }
 
-const fontWeight = postcss.plugin('fontWeight', () => {
-    return function (css) {
+const fontWeight = () => ({
+    postcssPlugin: 'fontWeight',
+    Once(css) {
         css.walkDecls('font-weight', decl => {
             const {value} = decl;
             decl.value = value === 'normal' ? '400' : value === 'bold' ? '700' : value;
         });
-    };
+    },
 });
+fontWeight.postcss = true;
 
-test('readme example code', t => {
-    const counter = postcss.plugin('counter', () => {
-        return css => {
+test('readme example code', () => {
+    const counter = () => ({
+        postcssPlugin: 'counter',
+        Once(css) {
             css.walkDecls('foo', decl => {
                 let value = parseInt(decl.value, 10);
                 value += 1;
                 decl.value = String(value);
             });
-        };
+        },
     });
+    counter.postcss = true;
 
     const css = 'h1 { foo: 1 }';
-    const out = postcss([ filter(), counter(), counter() ]).process(css).css;
-    t.is(out, 'h1 { foo: 2 }');
+    const out = postcss([ filter(), counter(), counter() ]).process(css, {from: undefined}).css;
+    assert.equal(out, 'h1 { foo: 2 }');
 });
 
-test('should handle processors without the postcssPlugin property', t => {
+test('should handle processors without the postcssPlugin property', () => {
     const foo = () => css => css.walkDecls('foo', f => f.remove());
     const css = 'h1 { foo: bar; font-weight: normal }';
 
-    return postcss([ foo(), fontWeight(), filter(), foo(), fontWeight() ]).process(css).then(result => {
-        t.is(result.css, 'h1 { font-weight: 400 }');
-        t.is(result.processor.plugins.length, 4);
+    return postcss([ foo(), fontWeight(), filter(), foo(), fontWeight() ]).process(css, {from: undefined}).then(result => {
+        assert.equal(result.css, 'h1 { font-weight: 400 }');
+        assert.equal(result.processor.plugins.length, 4);
     });
 });
 
-test('should warn if plugin exists', t => {
+test('should warn if plugin exists', () => {
     const plugins = [
         fontWeight(),
         filter(),
         fontWeight(),
     ];
     const warnings = getWarnings(plugins);
-    t.is(warnings.length, 1);
-    t.is(warnings[0].text, 'Found duplicate plugin: fontWeight');
+    assert.equal(warnings.length, 1);
+    assert.equal(warnings[0].text, 'Found duplicate plugin: fontWeight');
 });
 
-test('should warn if plugin exists, with a custom message', t => {
+test('should warn if plugin exists, with a custom message', () => {
     const template = ({postcssPlugin}) => `Uh-oh: ${postcssPlugin}`;
     const plugins = [
         fontWeight(),
@@ -61,54 +68,54 @@ test('should warn if plugin exists, with a custom message', t => {
         fontWeight(),
     ];
     const warnings = getWarnings(plugins);
-    t.is(warnings.length, 1);
-    t.is(warnings[0].text, 'Uh-oh: fontWeight');
+    assert.equal(warnings.length, 1);
+    assert.equal(warnings[0].text, 'Uh-oh: fontWeight');
 });
 
-test('should not warn when silent', t => {
+test('should not warn when silent', () => {
     const plugins = [
         fontWeight(),
         filter({silent: true}),
         fontWeight(),
     ];
-    t.is(getWarnings(plugins).length, 0);
+    assert.equal(getWarnings(plugins).length, 0);
 });
 
-test('should not warn if the plugin is excluded from the filter', t => {
+test('should not warn if the plugin is excluded from the filter', () => {
     const plugins = [
         fontWeight(),
         filter({exclude: ['fontWeight']}),
         fontWeight(),
     ];
-    t.is(getWarnings(plugins).length, 0);
+    assert.equal(getWarnings(plugins).length, 0);
 });
 
-test('should operate forward from the filter instance', t => {
+test('should operate forward from the filter instance', () => {
     const plugins = [
         fontWeight(),
         filter({direction: 'forward'}),
         fontWeight(),
         fontWeight(),
     ];
-    t.is(getWarnings(plugins).length, 1);
+    assert.equal(getWarnings(plugins).length, 1);
 });
 
-test('should operate backward from the filter instance', t => {
+test('should operate backward from the filter instance', () => {
     const plugins = [
         fontWeight(),
         fontWeight(),
         filter({direction: 'backward'}),
         fontWeight(),
     ];
-    t.is(getWarnings(plugins).length, 1);
+    assert.equal(getWarnings(plugins).length, 1);
 });
 
-test('should not filter itself when there are multiple instances', t => {
+test('should not filter itself when there are multiple instances', () => {
     const plugins = [ filter(), filter(), filter(), filter() ];
-    t.falsy(getWarnings(plugins).length);
+    assert.ok(!getWarnings(plugins).length);
 });
 
-test('should use the postcss plugin api', t => {
-    t.truthy(filter().postcssVersion, 'should be able to access version');
-    t.deepEqual(filter().postcssPlugin, name, 'should be able to access name');
+test('should use the postcss plugin api', () => {
+    assert.ok(filter().postcssVersion, 'should be able to access version');
+    assert.deepEqual(filter().postcssPlugin, name, 'should be able to access name');
 });
